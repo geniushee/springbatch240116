49a280f994fe739a71c23c94fad039cc07a097e4
geniushee <h1116jeon@gmail.com>
2024-01-16 21:44:26 +0900

    Project(spring batch learning)  init

---
+++ src/main/java/com/ll/springbatch_240116/Springbatch240116Application.java
@@ -0,0 +1,13 @@
+package com.ll.springbatch_240116;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbatch240116Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbatch240116Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/springbatch_240116/Springbatch240116ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.springbatch_240116;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbatch240116ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
